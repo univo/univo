80fba7cee831fec861eb6c2c70c1813e338810ba
Sam Potter <sam.potter@hotmail.com>
2026-03-12 13:25:43 +1100

    Use random port to avoid port collisions

--- packages/univo/mocks/handlers.ts
+++ packages/univo/mocks/handlers.ts
@@ -4,6 +4,6 @@ import { http, passthrough } from "msw";
 config(); // Ensure the env variables are correctly loaded
 
 export const handlers = [
-	http.all("http://localhost:3000/:key", passthrough), //
+	http.all("http://localhost:7483/:key", passthrough), //
 	http.all(process.env.TEST_ETHEREUM_RPC_URL, passthrough),
 ];
