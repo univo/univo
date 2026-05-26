0068240275957e394dbb3e5ff3cda588be911165
Sam Potter <sam.potter@hotmail.com>
2026-05-26 15:19:43 +1000

    Fix

--- examples/realtime/index.ts
+++ examples/realtime/index.ts
@@ -7,5 +7,5 @@ if (!process.env.INDEXER_URL) throw new Error("INDEXER_URL is not defined");
 realtime({
 	quiet: false,
 	node: wss(process.env.RPC_URL),
-	indexer: http(process.env.ENDPOINT_URL),
+	indexer: http(process.env.INDEXER_URL),
 });
