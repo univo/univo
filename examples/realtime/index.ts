df08cd854d7fe1589660765842b1b100d6b0461a
Sam Potter <sam.potter@hotmail.com>
2026-05-26 15:16:09 +1000

    Update realtime example

--- examples/realtime/index.ts
+++ examples/realtime/index.ts
@@ -1,9 +1,11 @@
-import { realtime, defineTransport } from "univo/realtime";
+import { realtime } from "univo/realtime";
+import { http, wss } from "univo/transport";
 
 if (!process.env.ENDPOINT_URL) throw new Error("ENDPOINT_URL is not defined");
 if (!process.env.TRANSPORT_URL) throw new Error("TRANSPORT_URL is not defined");
 
 realtime({
-	endpoints: [process.env.ENDPOINT_URL],
-	transport: defineTransport(process.env.TRANSPORT_URL),
+	quiet: false,
+	node: wss(process.env.TRANSPORT_URL),
+	indexer: http(process.env.ENDPOINT_URL),
 });
