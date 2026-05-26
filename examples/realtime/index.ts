261716eddccf923a350f0344bba3a3ed14d5adbc
Sam Potter <sam.potter@hotmail.com>
2026-05-26 15:20:08 +1000

    Cs

--- examples/realtime/index.ts
+++ examples/realtime/index.ts
@@ -1,11 +1,11 @@
 import { realtime } from "univo/realtime";
 import { http, wss } from "univo/transport";
 
-if (!process.env.RPC_URL) throw new Error("RPC_URL is not defined");
+if (!process.env.NODE_URL) throw new Error("NODE_URL is not defined");
 if (!process.env.INDEXER_URL) throw new Error("INDEXER_URL is not defined");
 
 realtime({
 	quiet: false,
-	node: wss(process.env.RPC_URL),
+	node: wss(process.env.NODE_URL),
 	indexer: http(process.env.INDEXER_URL),
 });
