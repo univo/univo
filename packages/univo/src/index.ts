d918706c1df42c352876a2a8a834b3e0a6316e23
Sam Potter <sam.potter@hotmail.com>
2026-09-18 15:58:27 +1000

    Load block from chain

--- packages/univo/src/index.ts
+++ packages/univo/src/index.ts
@@ -808,10 +808,8 @@ function indexer<TBlock extends Block>(opts: IndexerOptions<TBlock>) {
 
 		const manifestKey = `manifest/v1/${normalizeHex(head.chain)}`;
 
-		// TODO: There is no guarantee that block loaded from metadata is canonical
-
 		const [block, manifestRes, chainFinalizedBlock] = await Promise.all([
-			getBlockFromMetadataOrChain(head),
+			getBlockFromChain(head),
 			opts.metadataStorage.adapter.get(manifestKey),
 			getBlockFromChain({ chain: head.chain, number: "finalized" }),
 		]);
