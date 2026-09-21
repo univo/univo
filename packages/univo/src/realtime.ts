8cf70dc520811d9c521e78b5cca97c05be12e307
Sam Potter <sam.potter@hotmail.com>
2026-09-21 15:17:51 +1000

    Modify retries

--- packages/univo/src/realtime.ts
+++ packages/univo/src/realtime.ts
@@ -161,7 +161,7 @@ function defineBlockchain(opts: BlockchainOptions): Blockchain {
 		}
 
 		// Load the parent remote block and reconcile
-		const parentBlock = await retry(() => opts.getBlockByHash(newBlock.parent_hash), 5);
+		const parentBlock = await retry(() => opts.getBlockByHash(newBlock.parent_hash), 2);
 
 		if (parentBlock === null) {
 			throw new Error(`Failed to fetch parent block ${hexToNumber(newBlock.number)} ${newBlock.parent_hash.slice(0, 16)}`);
